c90ac6420e651b6cdfb20172b2ed2f6ee5e3a995
rezadyk <safri.110798@gmail.com>
2020-12-03 00:25:57 +0700

    initial commit

---
+++ src/elements/Star/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Star({ className, value, height, width, spacing }) {
+  const decimals = Number(value) % 1;
+
+  const star = {};
+  let leftPos = 0;
+  for (let index = 0; index < 5 && index < value - decimals; index++) {
+    leftPos = leftPos + width;
+    star.push(
+      <div
+        className="star"
+        key={`star-${index}`}
+        style={{ left: index * width, width: width, marginRight: spacing }}
+      ></div>
+    );
+  }
+
+  if (decimals > 0 && value <= 5) {
+    star.push(
+      <div
+        className="star"
+        key={`star-${index}`}
+        style={{ left: leftPos, width: width - spacing }}
+      ></div>
+    );
+  }
+
+  const starPlaceholder = [];
+  for (let index = 0; index < 5; index++) {
+    starPlaceholder.push(
+      <div
+        className="star placeholder"
+        key={`starPlaceholder-${index}`}
+        style={{ left: index * width, width: width, marginRight: spacing }}
+      ></div>
+    );
+  }
+
+  return (
+    <>
+      <div
+        className={["stars", className].join(" ")}
+        style={{ height: height }}
+      >
+        {starPlaceholder}
+        {star}
+      </div>
+    </>
+  );
+}
+
+Star.PropTypes = {
+  className: PropTypes.string,
+  value: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  spacing: PropTypes.number,
+};
