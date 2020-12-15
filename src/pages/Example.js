6d0f96a399c73a01ebd8503de4ab3e32a0a07399
rezadyk <safri.110798@gmail.com>
2020-12-15 17:59:05 +0700

    date range

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-import InputNumber from "elements/Form/InputNumber";
+import InputDate from "elements/Form/InputDate";
 
 export default class Example extends Component {
   state = {
-    value: "1",
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
   };
 
   handleChange = (e) => {
@@ -18,10 +22,8 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputNumber
+            <InputDate
               max={30}
-              suffix=" night"
-              isSuffixPlural
               onChange={this.handleChange}
               name="value"
               value={this.state.value}
